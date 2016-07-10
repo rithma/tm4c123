cb8669a39c781e647e400de4ec0c0b443c9dbb05
rithma <rithers@gmail.com>
2016-07-10 12:08:47 -0700

    Update LAB_8_6.c

--- LAB_8_6.c
+++ LAB_8_6.c
@@ -1,14 +1,16 @@
-
 /*
+
+BUILT IN THE FOLDER FOR SDADCPOLL_MINE for make all files work....
+please rebuild folder... etc
+
 built up around Lab 8.6 in Ying Bai's book 'Practical Engineering with ARM technology'
-- it going to take some time,   but this is in effort to learn how to use API functions,
--like SysCtlPeripheralEnable()     when i dont really grasp what to put after the parenthesis.
+
 */
 
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
-#include "driverlib.h"
+#include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
@@ -24,14 +26,14 @@ bool intFlag = true;
 
 int main (void) {
   char s_data;
-  int rcdata[26];
-  
-//3. Use SysCtlPeripheralEnable() to enable clock and GPIO Port B
-SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB | SYSCTL_PERIPJ_GPIOF);
-SysCtlPeripheralEnable(UART1);
+  int	 rcdata[26];
 
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB | SYSCTL_PERIPH_GPIOF);
+SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+GPIOPinTypeGPIOOutput	(GPIO_PORTF_BASE, GPIO_PIN_1);
+GPIOPinTypeUART				(GPIO_PORTB_BASE, GPIO_PIN_1 | GPIO_PIN_0);
+GPIOPinConfigure			(GPIO_PB1_U1TX | GPIO_PB0_U1RX);
 
-  
   
 }
 
