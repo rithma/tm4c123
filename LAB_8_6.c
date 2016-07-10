a6324ed401cdf5a8593468edd1033a127f703e61
rithma <rithers@gmail.com>
2016-07-09 23:28:01 -0700

    Create LAB_8_6.c

---
+++ LAB_8_6.c
@@ -0,0 +1,17 @@
+
+/*
+built up around Lab 8.6 in Ying Bai's book 'Practical Engineering with ARM technology'
+- it going to take some time,   but this is in effort to learn how to use API functions,
+-like SysCtlPeripheralEnable()     when i dont really grasp what to put after the parenthesis.
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+#define GPIO_PB0_U1RX     0x00010001
+#define GPIO_PB1_U1TX     0x00010401
+#define INT_UART1         22
