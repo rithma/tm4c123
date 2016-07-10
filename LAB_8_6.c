2133c6b684877d5222cf258f8e68d2aac51e313f
rithma <rithers@gmail.com>
2016-07-09 23:31:49 -0700

    Update LAB_8_6.c

--- LAB_8_6.c
+++ LAB_8_6.c
@@ -3,6 +3,7 @@
 built up around Lab 8.6 in Ying Bai's book 'Practical Engineering with ARM technology'
 - it going to take some time,   but this is in effort to learn how to use API functions,
 -like SysCtlPeripheralEnable()     when i dont really grasp what to put after the parenthesis.
+*/
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -15,3 +16,18 @@ built up around Lab 8.6 in Ying Bai's book 'Practical Engineering with ARM techn
 #define GPIO_PB0_U1RX     0x00010001
 #define GPIO_PB1_U1TX     0x00010401
 #define INT_UART1         22
+
+void UARTIntHandler(void);
+char rdata[26];
+uint8_t rnum = 0;
+bool intFlag = true;
+
+int main (void) {
+  char s_data;
+  int rcdata[26];
+  
+//3. Use SysCtlPeripheralEnable() to enable clock and GPIO Port B
+  
+  
+}
+
