0c61a3a3322c8ee0752fb2faf86791844a5b47b5
rithma <rithers@gmail.com>
2016-07-13 20:11:57 -0700

    Update temperature_sensor.c

--- temperature_sensor.c
+++ temperature_sensor.c
@@ -77,40 +77,13 @@ void Wait(uint32_t time){
 void
 InitConsole(void)
 {
-    //
-    // Enable GPIO port A which is used for UART0 pins.
-    // TODO: change this to whichever GPIO port you are using.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    //
-    // Configure the pin muxing for UART0 functions on port A0 and A1.
-    // This step is not necessary if your part does not support pin muxing.
-    // TODO: change this to select the port/pin you are using.
-    //
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-
-    //
-    // Enable UART0 so that we can configure the clock.
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-
-    //
-    // Use the internal 16MHz oscillator as the UART clock source.
-    //
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-
-    //
-    // Select the alternate (UART) function for these pins.
-    // TODO: change this to select the port/pin you are using.
-    //
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-
-    //
-    // Initialize the UART for console I/O.
-    //
-    UARTStdioConfig(0, 115200, 16000000);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);     //select UART GPIO bank
+    GPIOPinConfigure(GPIO_PA0_U0RX);                 //configures pin mux
+    GPIOPinConfigure(GPIO_PA1_U0TX);                 //configures pin mux
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);    //enable UART0 to config clock
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);//uses 16000000 clock for uart clock
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);   //uart pins
+    UARTStdioConfig(0, 115200, 16000000);           //initialize uart for condole i/o
 }
 
 int main(){
@@ -130,28 +103,15 @@ int main(){
 	    // was used with a deeper FIFO, then the array size must be changed.
 	    //
 
-	    //
-	    // These variables are used to store the temperature conversions for
-	    // Celsius and Fahrenheit.
-	    //
-
-
-	    //
-	    // Display the setup on the console.
-	    //
 	    UARTprintf("ADC ->\n");
 	    UARTprintf("  Type: Internal Temperature Sensor\n");
 	    UARTprintf("  Samples: One\n");
 	    UARTprintf("  Update Rate: 250ms\n");
 	    UARTprintf("  Input Pin: Internal temperature sensor\n\n");
 
-	    //
-	    // The ADC0 peripheral must be enabled for use.
-	    //
-	    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);    //enable ADC
 	    SysCtlDelay(3);
 
-	    //
 	    // Enable sample sequence 3 with a processor signal trigger.  Sequence 3
 	    // will do a single sample when the processor sends a singal to start the
 	    // conversion.  Each ADC module has 4 programmable sequences, sequence 0
